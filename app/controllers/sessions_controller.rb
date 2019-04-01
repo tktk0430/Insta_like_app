1c44c1f77f13f2a8c1f995473c6d5d61ede80738
tktk0430 <h.taku.0430.tech@gmail.com>
2019-04-01 12:43:22 +0000

    modify some link

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,4 +2,8 @@ class SessionsController < ApplicationController
   def new
     @user=User.new
   end
+
+  def create
+
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,6 @@ class UsersController < ApplicationController
 
   def create
     @user=User.new(user_params)
-    binding.pry
     if @user.save
       redirect_to @user, notice: "ようこそ！"
     else
