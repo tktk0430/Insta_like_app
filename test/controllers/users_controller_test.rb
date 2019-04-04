b0e0fe52a72804f85be62aa2b7a861e2c605c5a6
tktk0430 <h.taku.0430.tech@gmail.com>
2019-04-04 05:52:03 +0000

    user_controller_test 追加

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,4 +1,80 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
+  
+  setup do
+    @tom=users(:tom)
+    @bob=users(:bob)
+  end
+
+  test "デフォルトネームは名無しさん" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post users_path,params:{user:{
+                              account: "hogehoge",
+                              email: "hogehoge@gmail.com",
+                              password: "password",
+                              password_confirmation: "password"}}
+    end
+    user=User.find_by(email: "hogehoge@gmail.com")
+    assert_equal user.name, "名無しさん", "ユーザーネームは#{user.name}"
+  end
+
+  test "user#indexはログイン必須" do
+    get users_path
+    assert_not flash.empty?
+    assert_redirected_to login_path
+    login_as(@tom)
+    get users_path
+    assert_template 'users/index'
+  end
+
+  test "user#showはログイン必須" do
+    get user_path(@tom)
+    assert_not flash.empty?
+    assert_redirected_to login_path
+    login_as(@tom)
+    get user_path(@tom)
+    assert_template 'users/show'
+  end
+
+  test "user#editはログインかつcorrect_user必須" do
+    get edit_user_path(@tom)
+    assert_not flash.empty?
+    assert_redirected_to login_path
+    login_as(@tom)
+    get edit_user_path(@bob)
+    assert_not flash.empty?
+    assert_redirected_to root_path
+    get edit_user_path(@tom)
+    assert_template 'users/edit'
+  end
+
+  test "user#updateはログインかつcorrect_user必須" do
+    patch user_path(@tom), params:{user:{name: "tomtom"}}
+    assert_not flash.empty?
+    assert_redirected_to login_path
+    login_as(@tom)
+    patch user_path(@bob), params:{user:{name: "tomtom"}}
+    assert_not flash.empty?
+    assert_redirected_to root_path
+    patch user_path(@tom), params:{user:{name: "tomtom"}}
+    assert_redirected_to @tom
+    @tom.reload
+    assert_equal "tomtom", @tom.name
+  end
+
+  test "user#destroyはログインかつcorrect_user必須" do
+    delete user_path(@tom)
+    assert_not flash.empty?
+    assert_redirected_to login_path
+    login_as(@tom)
+    delete user_path(@bob)
+    assert_not flash.empty?
+    assert_redirected_to root_path
+    assert_difference 'User.count', -1 do
+      delete user_path(@tom)
+    end
+    assert_redirected_to root_path
+  end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,6 +5,24 @@ require 'rails/test_help'
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
+  def is_logged_in?
+    !session[:user_id].nil?
+  end
 
+  def login_as(user)
+    session[:user_id] = user.id
+  end
   # Add more helper methods to be used by all tests here...
+end
+
+class ActionDispatch::IntegrationTest #Integration Test用
+  def login_as(user)
+    post login_path, params: { user: { email: user.email,
+                                          password: "password",
+                                          } }
+  end
+
+  def update(user,name:user.name, account:user.account, email:user.email)
+    
+  end
 end
