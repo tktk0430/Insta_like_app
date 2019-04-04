f8fa4e481211f982e41baec282542b2482f95d8a
tktk0430 <h.taku.0430.tech@gmail.com>
2019-04-04 17:22:13 +0000

    sitelayouttest追加

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   resources :sessions
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 
   resource :password
 end
--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @tom=users(:tom)
+    @bob=users(:bob)
+  end
+
+  test "home/topのログイン状態によるビュー確認" do
+    get root_path
+    assert_template "home/top"
+    assert_select "a[href=?]", root_path
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", signup_path
+    assert_select "a[href=?]", login_path
+    login_as(@tom)
+    assert_redirected_to @tom
+    get root_path
+    assert_select "a[href=?]", root_path
+    assert_select "a[href=?]", user_path(@tom)
+    assert_select "a[href=?]", users_path
+    assert_select "a[href=?]", logout_path
+  end
 end
