bf7835c7f79e67b7d86153700a5339a57029a662
tktk0430 <h.taku.0430.tech@gmail.com>
2019-04-06 06:19:42 +0000

    micropost new 実装

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,9 +1,22 @@
 class MicropostsController < ApplicationController
+  before_action :login_user?
+
   def new
-    @micropost=Micropost.new
+    @micropost=current_user.microposts.build
   end
 
   def create
-
+    @micropost=current_user.microposts.build(micropost_params)
+    if @micropost.save
+      flash[:success] = "画像を投稿しました"
+      redirect_to current_user
+    else
+      render 'new'
+    end
   end
+
+  private
+    def micropost_params
+      params.require(:micropost).permit(:content)
+    end
 end
