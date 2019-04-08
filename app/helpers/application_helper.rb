3dbdc2de2607475b4ae9a14623ddcf0ea9b4d89a
tktk0430 <h.taku.0430.tech@gmail.com>
2019-04-08 06:47:53 +0000

    レスポンシブ対応

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,12 @@ module ApplicationHelper
   def page_title
     !@page_title.nil? ? "#{@page_title} | Insta App" : "Insta App"
   end
+
+  def logo
+    if login?
+      link_to 'INSTA_APP', root_path, id:'logo', class:'none'
+    else
+      link_to 'INSTA_APP', root_path, id:'logo'
+    end
+  end
 end
