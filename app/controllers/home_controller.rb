3822262f69c46f9e195acc5c27ec959c06a57795
tktk0430 <h.taku.0430.tech@gmail.com>
2019-04-08 13:26:52 +0000

    feed実装

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,7 @@ class HomeController < ApplicationController
   
   def top
     current_user.nil? ? @user=User.new : @user=current_user
+    @microposts=Micropost.where("user_id IN (?) OR user_id = ?", @user.following_ids, @user.id).page(params[:page]).per(12)
   end
 
   def about
