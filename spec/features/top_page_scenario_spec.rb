43e8c239dfb349de9517e0fad84287833226f237
tktk0430 <h.taku.0430.tech@gmail.com>
2019-05-02 17:55:19 +0000

    top_page_scenario かんたん新規登録&かんたんログイン追加

--- spec/features/top_page_scenario_spec.rb
+++ spec/features/top_page_scenario_spec.rb
@@ -47,8 +47,19 @@ RSpec.feature "TopPageScenarios", type: :feature do
   end
 
   context 'ログイン' do
+    #デフォルト画像を持ったユーザーを作るため
     before do
-      create_new_user_from_form
+      visit root_path
+      click_link '新規登録'
+      fill_in '名前', with: 'newname'
+      fill_in 'ユーザーネーム', with: 'newaccount'
+      fill_in 'メールアドレス', with: 'newmail@example.com'
+      fill_in 'パスワード', with: 'hogehoge'
+      fill_in 'パスワード（確認用）', with:'hogehoge'
+      click_button '新規登録'
+      click_link '設定'
+      click_link 'ログアウト'
+      @user=User.find_by(email:'newmail@example.com')
     end
 
     scenario '成功' do
@@ -56,8 +67,7 @@ RSpec.feature "TopPageScenarios", type: :feature do
       click_link 'ログイン'
       fill_in 'メールアドレス', with: 'newmail@example.com'
       fill_in 'パスワード', with: 'hogehoge'
-      click_button 'ログイン'
-      expect(page).to have_content 'newname'
+      expect{click_button 'ログイン'}.to change {current_path}.to(user_path(@user))
     end
 
     scenario '失敗' do
@@ -65,22 +75,39 @@ RSpec.feature "TopPageScenarios", type: :feature do
       click_link 'ログイン'
       fill_in 'メールアドレス', with: ''
       fill_in 'パスワード', with: 'hogehoge'
-      click_button 'ログイン'
+      expect{click_button 'ログイン'}.to_not change {current_path}
       expect(page).to have_content 'メールアドレスとパスワードが一致しません'
     end
-
   end
 
-  def create_new_user_from_form
-    visit root_path
-    click_link '新規登録'
-    fill_in '名前', with: 'newname'
-    fill_in 'ユーザーネーム', with: 'newaccount'
-    fill_in 'メールアドレス', with: 'newmail@example.com'
-    fill_in 'パスワード', with: 'hogehoge'
-    fill_in 'パスワード（確認用）', with:'hogehoge'
-    click_button '新規登録'
-    click_link '設定'
-    click_link 'ログアウト'
+  context 'かんたん新規登録' do
+    scenario '成功' do
+      visit root_path
+      expect{click_link 'かんたん新規登録'}.to change(User.all,:count).by(1)
+      user=User.first
+      expect(user.name).to include 'ご新規さま'
+      expect(current_path).to eq user_path(user)
+    end
+  end
+
+  context 'かんたんログイン' do
+    before do
+      visit root_path
+      click_link '新規登録'
+      fill_in '名前', with: 'テスト太郎'
+      fill_in 'ユーザーネーム', with: 'tester'
+      fill_in 'メールアドレス', with: 'test@example.com'
+      fill_in 'パスワード', with: 'password'
+      fill_in 'パスワード（確認用）', with:'password'
+      click_button '新規登録'
+      click_link '設定'
+      click_link 'ログアウト'
+      @tester=User.find_by(email:'test@example.com')
+    end
+
+    scenario '成功' do
+      visit root_path
+      expect{click_link 'かんたんログイン'}.to change{current_path}.to(user_path(@tester))
+    end
   end
 end
