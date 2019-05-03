178d119cc80a323eff9fdb73e792a2c75a1a62e1
tktk0430 <h.taku.0430.tech@gmail.com>
2019-05-03 15:47:21 +0000

    micropostモデルテスト作成

--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -5,4 +5,40 @@ RSpec.describe Micropost, type: :model do
     post=FactoryBot.build(:micropost)
     expect(FactoryBot.build(:micropost)).to be_valid
   end
+
+  it "はid降順に並ぶ" do
+    post1=FactoryBot.create(:micropost)
+    post2=FactoryBot.create(:micropost)
+    post3=FactoryBot.create(:micropost)
+    expect(Micropost.first).to eq post3
+  end
+
+  it "は本文が長すぎると無効" do
+    post=FactoryBot.build(:micropost,content:'a'*241)
+    expect(post).not_to be_valid
+  end
+
+  it "は本文がなくても有効" do
+    post=FactoryBot.build(:micropost,content:'')
+    expect(post).to be_valid
+  end
+
+  it "は画像がないと無効" do
+    post=FactoryBot.build(:micropost,image:nil)
+    expect(post).not_to be_valid
+  end
+
+  describe 'いいね！通知作成・削除' do
+    it "によりいいね！通知が送られる＆削除される" do
+      user=FactoryBot.create(:user)
+      another=FactoryBot.create(:user)
+      micropost=FactoryBot.create(:micropost,contributer:another)
+      micropost.create_notification_by(user)
+      notif=Notification.find_by(visiter:user.id,micropost:micropost.id)
+      expect(notif).not_to be_nil
+      micropost.delete_notification_by(user)
+      notif=Notification.find_by(visiter:user.id,micropost:micropost.id)
+      expect(notif).to be_nil
+    end
+  end
 end
