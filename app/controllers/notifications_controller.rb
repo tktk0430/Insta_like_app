3ea333f95287e2a906505c3f95de0a51ed052a87
tktk0430 <h.taku.0430.tech@gmail.com>
2019-05-24 11:28:22 +0000

    通知のn+1問題回避

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -3,7 +3,7 @@ class NotificationsController < ApplicationController
 
   def index
     @user=current_user
-    @notifications=current_user.passive_notifications.page(params[:page]).per(10)
+    @notifications=current_user.passive_notifications.page(params[:page]).per(10).includes(:visiter,:micropost)
     @notifications.where(checked: false).each do |notification|
       notification.update_attributes(checked: true)
     end
